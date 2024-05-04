048c685fba9924ad9a77e4b81862cd2a2df00846
Kmaldon <A01252830@itesm.mx>
2024-05-03 20:08:10 -0700

    Fixed "Save" button

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -50,8 +50,8 @@ const Task: React.FC<TaskProps> = ({ taskis }) => {
                             <div>
                                 <h3 className="mb-5 text-lg font-normal">Edit the selected task</h3>
                                 <input value={TaskToEdit} onChange={(e) => setTaskToEdit(e.target.value)} type="task" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-s focus:ring-blue-500 focus:border-blue-500" placeholder="Edit Task" />
-                                <button type="button" className="focus:outline-none text-white bg-green-600 hover:bg-green-400 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2">
-                                    {"Save"}
+                                <button type="submit" className="focus:outline-none text-white bg-green-600 hover:bg-green-400 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2">
+                                    Save
                                 </button>
                                 <button onClick={() => setModalOpenEdit(false)} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2">
                                     Cancel
